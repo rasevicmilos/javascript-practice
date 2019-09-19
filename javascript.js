ca4d16d51ae3fe39331cfe741393f6b265159eef
Milos <milos.rasevic@vivifyideas.com>
2019-09-19 10:22:28 +0200

    Variable scope

---
+++ javascript.js
@@ -0,0 +1,16 @@
+var a = 5;
+
+function func() {
+    var b = 10;
+    for(i = 0; i < 10; i++) {
+        var x = 1;
+        let y = 1;
+    }
+    console.log(x);
+    // console.log(y);
+    console.log(a);
+}
+
+// console.log(b);
+
+func();
