54177778df8c452bfbbfe6018f265df6e22c4923
Milos <milos.rasevic@vivifyideas.com>
2019-09-19 11:55:24 +0200

    JS/JSON Objects

--- javascript.js
+++ javascript.js
@@ -1,16 +1,32 @@
-var a = 5;
-
-function func() {
-    var b = 10;
-    for(i = 0; i < 10; i++) {
-        var x = 1;
-        let y = 1;
-    }
-    console.log(x);
-    // console.log(y);
-    console.log(a);
+var person = {
+    firstName: 'Milos',
+    lastName: 'Rasevic',
+    age: 22
 }
 
-// console.log(b);
+var personAsJSON = JSON.stringify(person);
 
-func();
+var personAsObj = JSON.parse(personAsJSON);
+
+console.log('Person as JSON');
+console.log('-------------------------------------------------');
+console.log(personAsJSON);
+console.log('-------------------------------------------------');
+console.log('Person as Object');
+console.log('-------------------------------------------------');
+console.log(personAsObj);
+console.log('-------------------------------------------------');
+
+var jsNumbers = [1,2,3,4,5,6,7];
+
+var JSON_numbers = "[ 1, 2, 3, 4, 5, 6, 7 ]";
+
+var JSON_to_JS_numbers = JSON.parse(JSON_numbers);
+
+console.log('Loop over parsed numbers');
+console.log('-------------------------------------------------');
+
+for (number in JSON_to_JS_numbers) {
+    console.log(JSON_to_JS_numbers[number]);
+}
+console.log('-------------------------------------------------');
